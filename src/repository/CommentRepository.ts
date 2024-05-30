0f29e99820797a0a8f9d62958c25c16700310bd3
kali-vinnivso <kali-vinnivso@kali-vinnivso.kali-vinnivso>
2024-05-30 20:20:26 -0300

    Develop: Comment Repository

---
+++ src/repository/CommentRepository.ts
@@ -0,0 +1,41 @@
+import { CommentDB } from "../model/Comment/CommentInterface";
+import { BaseRepository } from "./BaseRepository";
+
+export class CommentRepository extends BaseRepository {
+  private static COMMENTPOST_TABLE = "comment_post";
+
+  public createComment = async (comment: CommentDB): Promise <void> => {
+    await BaseRepository
+      .connection(CommentRepository.COMMENTPOST_TABLE)
+      .insert(comment);
+  }
+
+  public findCommentById = async (id: string): Promise <CommentDB[]> => {
+    const result: CommentDB[] = await BaseRepository
+      .connection(CommentRepository.COMMENTPOST_TABLE)
+      .where({ id });
+    return result;
+  }
+
+  public findCommentByPostId = async (id: string): Promise <CommentDB[]> => {
+    const result: CommentDB[] = await BaseRepository
+      .connection(CommentRepository.COMMENTPOST_TABLE)
+      .where({ id_post: id });
+    return result;
+  }
+
+  public editComment = async (comment: CommentDB, id: string): Promise <any> => {
+    await BaseRepository
+      .connection(CommentRepository.COMMENTPOST_TABLE)
+      .update(comment)
+      .where({ id });
+  }
+
+  public deleteComment = async (id: string): Promise <any> => {
+    await BaseRepository
+      .connection(CommentRepository.COMMENTPOST_TABLE)
+      .del()
+      .where({ id });
+  }
+
+}
