0d00bb7e761d6605a47882e6ebab24a8c37ba234
kali-vinnivso <kali-vinnivso@kali-vinnivso.kali-vinnivso>
2024-05-30 19:42:32 -0300

    Develop: LikeDislikePost model

---
+++ src/model/LikeDislikePost.ts
@@ -0,0 +1,49 @@
+export interface LikeDislikePostDB {
+  id_user: string;
+  id_post: string;
+  like: number | null;
+}
+
+export interface LikeOrDislikePost {
+  idUser: string;
+  like: number | null;
+}
+
+export class LikeDislikePost {
+  constructor(
+    private idUser: string,
+    private idPost: string,
+    private like: number | null
+  ) { }
+
+  public get IDUSER(): string {
+    return this.idUser;
+  }
+
+  public get IDPOST(): string {
+    return this.idPost;
+  }
+
+  public get LIKE(): number | null {
+    return this.like;
+  }
+
+  public set IDUSER(newIdUSer: string) {
+    this.idUser = newIdUSer;
+  }
+
+  public set IDPOST(newIdPost: string) {
+    this.idPost = newIdPost;
+  }
+
+  public set LIKE(newLike: number | null) {
+    this.like = newLike;
+  }
+
+  public LikeDislikeToResult(): LikeOrDislikePost {
+    return {
+      idUser: this.idUser,
+      like: this.like,
+    };
+  }
+}
