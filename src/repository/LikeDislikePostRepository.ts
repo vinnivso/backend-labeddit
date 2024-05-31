dcf95225f8ef41d74744ea0796985301beda6e18
kali-vinnivso <kali-vinnivso@kali-vinnivso.kali-vinnivso>
2024-05-30 21:50:20 -0300

    Develop: LikeDislikeRepository

---
+++ src/repository/LikeDislikePostRepository.ts
@@ -0,0 +1,44 @@
+import { LikeOrDislikePostDB } from "../model/LikeDislike/LikeDislikePostInterface";
+import { BaseRepository } from "./BaseRepository";
+
+export class LikeDislikePostRepository extends BaseRepository {
+  private static LIKESDISLIKES_TABLE = "like_dislike";
+
+  public newLikeOrDislikePost = async (likeDislike: LikeOrDislikePostDB): Promise<void> => {
+    await BaseRepository
+      .connection(LikeDislikePostRepository.LIKESDISLIKES_TABLE)
+      .insert(likeDislike);
+  };
+
+  public findLikeDislikePostById = async (id_post: string, id_user: string): Promise<LikeOrDislikePostDB[]> => {
+    const result: LikeOrDislikePostDB[] = await BaseRepository
+      .connection(LikeDislikePostRepository.LIKESDISLIKES_TABLE)
+      .where({ id_post })
+      .andWhere({ id_user });
+    return result;
+  };
+
+  public findLikeDislikeByIdPost = async (id_post: string): Promise<LikeOrDislikePostDB[]> => {
+    const result: LikeOrDislikePostDB[] = await BaseRepository
+      .connection(LikeDislikePostRepository.LIKESDISLIKES_TABLE)
+      .where({ id_post });
+    return result;
+  };
+
+  public updateLikeDislikePost = async (id_user: string,
+    id_post: string,
+    likeDislike: any): Promise<any> => {
+    await BaseRepository
+      .connection(LikeDislikePostRepository.LIKESDISLIKES_TABLE)
+      .update(likeDislike)
+      .where({ id_post })
+      .andWhere({ id_user })
+  };
+
+  public deleteLikeDislikePost = async (id_post: string): Promise<any> => {
+    await BaseRepository
+      .connection(LikeDislikePostRepository.LIKESDISLIKES_TABLE)
+      .del()
+      .where({ id_post })
+  };
+}
